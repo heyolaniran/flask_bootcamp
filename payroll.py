9dcc3526d0f240310be04990023d584725b10a32
Morty <olaniranlaurent@gmail.com>
2024-12-20 20:05:11 +0100

    feat: alchemy initialization

---
+++ payroll.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///payroll.db"
+db = SQLAlchemy(app)
+
+class Payroll(db.Model): 
+    id = db.Column(db.Integer(), primary_key=True)
+    base_name = db.Column(db.String(255), nullable=False)
+    base_url= db.Column(db.String(255), nullable=False)
+    file_path = db.Column(db.String(255), nullable=False, unique=True)
+
+@app.cli.command('create_db')
+def create_db():
+    """Create the database and tables."""
+    db.create_all()
+    print("Database created!")
+    
+@app.route('/')
+def home_page(): 
+    return render_template('home.html', page_name="Home")
+
+@app.route('/payroll')
+
+def payroll_page(): 
+    return render_template('payroll.html', page_name="Payroll")
+
+"""@app.route('/taxes/<society>')
+def compute_tax(society) :
+    return f'Here we gonna implements tax computation for {society}'
+"""
